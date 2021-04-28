8f95e8de659e0e61eec48756d5593b3993aa42d8
unknown <andhika@socketspace.com>
2021-04-28 20:54:59 +0700

    second commit

---
+++ asset/config.js
@@ -0,0 +1,3 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C09R3D2AIIH35PFCBF3G").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true});
+window[window["TiktokAnalyticsObject"]].instance("C09R3D2AIIH35PFCBF3G").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C09R3D2AIIH35PFCBF3G").setPixelInfo({status: 0, name: "Tokotalk - Indonesia", advertiserID: "6789845012914896902", setupMode: 1, partner: ""});
